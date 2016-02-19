e28d4d0655ed5bb815caebb5757e06c6f7b8e3b0
clempat <clement.patout@gmail.com>
2016-02-19 17:53:51 +0100

    😷 Try to fix the permission problem for twig files - maybe better solution

--- tasks/watch.js
+++ tasks/watch.js
@@ -4,12 +4,12 @@ import livereload from 'gulp-livereload';
 gulp.task('watch', () => {
   livereload.listen();
 
-  gulp.watch('./src/libraries/**/*.js', ['javascript']);
-  gulp.watch('./src/**/*.scss', ['sass']);
-  gulp.watch('./src/templates/*', ['template']);
-  gulp.watch('./src/*.yml', ['yml']);
+  gulp.watch('src/libraries/**/*.js', ['javascript']);
+  gulp.watch('src/**/*.scss', ['sass']);
+  gulp.watch('src/templates/*', ['template']);
+  gulp.watch('src/*.yml', ['yml']);
 
-  gulp.watch('./build/**/*', (files) => {
+  gulp.watch('build/**/*', (files) => {
     livereload.changed(files);
-  })
-})
+  });
+});
--- tasks/yml.js
+++ tasks/yml.js
@@ -2,8 +2,8 @@ import gulp from 'gulp';
 import replace from 'gulp-replace';
 
 gulp.task('yml', () => {
-  return gulp.src('./src/*.yml')
+  return gulp.src('src/*.yml')
     .pipe(replace(/[^\s]+\/([^\/]+\.js)/g, 'js/$1'))
     .pipe(replace(/[^\s]+\/([^\/]+\.css)/g, 'css/$1'))
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('build'));
 });
